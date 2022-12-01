a7774d49400634d2be4bd3822975e0386c1cce91
Denis Beluchenko <dzibis@gmail.com>
2022-12-01 09:15:27 +0700

    final

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
@@ -14,6 +15,27 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
-        System.out.println(persons);
+        Long minors = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        List<String> conscripts = persons.stream()
+                .filter(person -> person.getAge() < 27)
+                .filter(person -> person.getAge() > 18)
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+        List<Person> workMan = persons.stream()
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getAge() <= 65)
+                .filter(person -> person.getSex() == Sex.MAN)
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(person -> person.getFamily()))
+                .collect(Collectors.toList());
+        List<Person> workWoman = persons.stream()
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getAge() <= 60)
+                .filter(person -> person.getSex() == Sex.WOMAN)
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(person -> person.getFamily()))
+                .collect(Collectors.toList());
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,4 @@
- class Person {
+class Person {
     private String name;
     private String family;
     private Integer age;
